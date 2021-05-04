f6498c75f34be51cc6f79e7d90c3f82be30f7264
suhashoysala <shoysala@idelic.com>
2021-05-04 12:55:16 -0400

    Update file name, fix approach to all gulls

--- gull_tracking/gull_tracking_min_dist_all.py
+++ gull_tracking/gull_tracking_min_dist_all.py
@@ -14,34 +14,34 @@ def dist_diff(df):
      df['m_dist'].fillna(0 ,inplace = True)
      return df
 black_gull_path = Path(__file__).parent / "./data/LBBG_ZEEBRUGGE-gps-2013.csv"
-herring_gull_path = Path(__file__).parent / "./data/LBBG_ZEEBRUGGE-gps-2013.csv"
+herring_gull_path = Path(__file__).parent / "./data/HG_OOSTENDE-gps-2013.csv"
 black_gull = pandas.read_csv(black_gull_path.open())
 herring_gull = pandas.read_csv(herring_gull_path.open())
 
 all_gulls = pandas.concat([black_gull, herring_gull])
 
-black_gull['timestamp'] = pandas.to_datetime(black_gull.timestamp)
-black_gull_sorted = black_gull.sort_values(by=['timestamp'])
-black_gull_sorted['lat_long'] = black_gull_sorted[['location-lat', 'location-long']].apply(tuple, axis=1)
-bgs_dict = black_gull_sorted.to_dict('records')
+all_gulls['timestamp'] = pandas.to_datetime(black_gull.timestamp)
+all_gulls_sorted = all_gulls.sort_values(by=['timestamp'])
+all_gulls_sorted['lat_long'] = all_gulls_sorted[['location-lat', 'location-long']].apply(tuple, axis=1)
+ags_dict = all_gulls_sorted.to_dict('records')
 
 min_dists = []
 index = 0
 prev_id = ''
 prev_j = 0
-for index in range(len(bgs_dict)):
+for index in range(len(ags_dict)):
     dists = []
-    for j in range(index+1, len(bgs_dict)):
-        if bgs_dict[index]['individual-local-identifier'] == prev_id:
+    for j in range(index+1, len(ags_dict)):
+        if ags_dict[index]['individual-local-identifier'] == prev_id:
             j = prev_j
-        if (bgs_dict[index]['timestamp'] - bgs_dict[j]['timestamp']).total_seconds() > 1:
+        if (ags_dict[index]['timestamp'] - ags_dict[j]['timestamp']).total_seconds() > 1:
             break
-        if bgs_dict[index]['individual-local-identifier']!= bgs_dict[j]['individual-local-identifier']:
-            if abs((bgs_dict[index]['timestamp'] - bgs_dict[j]['timestamp']).total_seconds()) <= 1:
-                dists.append(haversine(bgs_dict[index]['lat_long'], bgs_dict[j]['lat_long'], unit = 'm'))
+        if ags_dict[index]['individual-local-identifier']!= ags_dict[j]['individual-local-identifier']:
+            if abs((ags_dict[index]['timestamp'] - ags_dict[j]['timestamp']).total_seconds()) <= 1:
+                dists.append(haversine(ags_dict[index]['lat_long'], ags_dict[j]['lat_long'], unit = 'm'))
             else:
                 break
-    prev_id = bgs_dict[index]['individual-local-identifier']
+    prev_id = ags_dict[index]['individual-local-identifier']
     prev_j = j
     print(f'Prev id was {str(prev_id)}, prev j was {str(j)}')
     print(f'At index {str(index)}')
