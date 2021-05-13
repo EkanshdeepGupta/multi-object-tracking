dac134aab3b90dd4adbf2652efa544885f375bed
Suhas Hoysala <shoysala@idelic.com>
2021-05-13 14:03:33 -0400

    Change to map

--- gull_tracking/gull_tracking_map.py
+++ gull_tracking/gull_tracking_map.py
@@ -2,7 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas
-import dateutil.parser
 from shapely.geometry import Point
 import geopandas as gpd
 from geopandas import GeoDataFrame
@@ -11,8 +10,8 @@ def dist_diff(df):
      df['m_dist'] = haversine(df, df.shift(), unit = 'm')
      df['m_dist'].fillna(0 ,inplace = True)
      return df
-black_gull = pandas.read_csv('/Users/suhashoysala/Downloads/gull_tracking/LBBG_ZEEBRUGGE-gps-2013.csv')
-herring_gull = pandas.read_csv('/Users/suhashoysala/Downloads/gull_tracking/HG_OOSTENDE-gps-2013.csv')
+black_gull = pandas.read_csv(r'C:\Users\14hoy\git\multi-object-tracking\gull_tracking\dataLBBG_ZEEBRUGGE-gps-2013.csv')
+herring_gull = pandas.read_csv(r'C:\Users\14hoy\git\multi-object-tracking\gull_tracking\data\HG_OOSTENDE-gps-2013.csv')
 
 all_gulls = pandas.concat([black_gull, herring_gull])
 
@@ -23,9 +22,11 @@ geometry = [Point(xy) for xy in zip(all_gulls['location-long'], all_gulls['locat
 
 gdf = GeoDataFrame(all_gulls, geometry=geometry) 
 
+gdf.groupby(gdf['individual-local-identifier'])
+
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
-gdf.plot(ax=world.plot(figsize=(10, 6)), marker='o', color='red', markersize=15)
+gdf.plot(ax=world.plot(figsize=(10, 6)), marker='+', markersize=15)
 
 plt.show()
 
