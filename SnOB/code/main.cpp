8105bd0fca2e160b062995e2bc2331d59b14776d
Ekanshdeep Gupta <ekanshdeep_gupta@yahoo.co.in>
2021-05-17 04:37:11 +0530

    updated second order model

---
+++ SnOB/code/main.cpp
@@ -0,0 +1,19 @@
+#include "Sn.hpp"
+#include "SnIrreducible.hpp"
+#include "SnElement.hpp"
+#include "Cycles.hpp"
+#include "SnFunction.hpp"
+#include "SnFourierTransform.hpp"
+#include "SnFtree.hpp"
+
+#include <iostream> 
+
+void find_largest_in_subgp(Sn::Element p, int k, int n) {
+    
+}
+
+int main() {
+
+
+    return 0;
+}
